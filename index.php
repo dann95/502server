8091d7f232e533712096c1a32cea57f1a63bf5e4
Daniel Rubin <daniel.rubin@colmeia.me>
2019-08-26 09:50:44 -0300

    php -S 0.0.0.0:8080 router.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+     return;
+}
+
+
+http_response_code(502);
